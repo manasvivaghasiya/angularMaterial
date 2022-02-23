4a8b4357b23b24cfaff1d68480f0df744ac56ca6
manasvivaghasiya <96326072+manasvivaghasiya@users.noreply.github.com>
2022-02-23 18:37:12 +0530

    angular

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-add',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.component.css']
 })
 export class AddComponent implements OnInit {
+  formControl: any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  confirmAdd(){
+
+  }
+  onNoClick(){
+
+  }
+  getErrorMessage() {
+    return this.formControl.hasError('required') ? 'Required field' :
+      this.formControl.hasError('email') ? 'Not a valid email' :
+        '';
+  }
 }
